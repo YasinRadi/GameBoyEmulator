731aaf9ce7965990844488fbcab5394a192fa828
Yasin Radi <yasin.ben.hamman@gmail.com>
2023-04-09 16:57:39 +0200

    feature/disassembler Add read and decode methods to Decoder class.

--- src/emulator/disassembler/decoder.py
+++ src/emulator/disassembler/decoder.py
@@ -1,7 +1,7 @@
 """Decoder class for the disassembler."""
 
 
-from pathlib import Path
+import sys
 from dataclasses import dataclass
 
 from config import PREFIXED, INSTRUCTIONS
@@ -12,14 +12,53 @@ class Decoder:
 
     data: bytes
     address: int
-    prefixed_instructions: dict
     instructions: dict
+    prefixed_instructions: dict
 
     @classmethod
     def create(cls, data: bytes, address: int = 0):
         return cls(
-            prefixed_instructions=PREFIXED,
-            instructions=INSTRUCTIONS,
             data=data,
             address=address,
+            instructions=INSTRUCTIONS,
+            prefixed_instructions=PREFIXED,
         )
+
+    def read(self, address: int, count: int = 1):
+        """
+        Reads `count` bytes starting from `address`.
+        """
+        if 0 <= address + count <= len(self.data):
+            v = self.data[address : address + count]
+            return int.from_bytes(v, sys.byteorder)
+        else:
+            raise IndexError(f'{address=}+{count=} is out of range')
+
+    def decode(self, address: int):
+        """
+        Decodes the instruction at `address`.
+        """
+        opcode = None
+        decoded_instruction = None
+        opcode = self.read(address)
+        address += 1
+        # 0xCB is a special prefix instruction. Read from
+        # prefixed_instructions instead and increment address.
+        if opcode == 0xCB:
+            opcode = self.read(address)
+            address += 1
+            instruction = self.prefixed_instructions[opcode]
+        else:
+            instruction = self.instructions[opcode]
+        new_operands = []
+        for operand in instruction.operands:
+            if operand.bytes is not None:
+                value = self.read(address, operand.bytes)
+                address += operand.bytes
+                new_operands.append(operand.copy(value))
+            else:
+                # No bytes; that means it's not a memory address
+                new_operands.append(operand)
+        decoded_instruction = instruction.copy(operands=new_operands)
+        return address, decoded_instruction
+    
