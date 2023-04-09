633fc361680473cd9a903c961597b0acab38cb86
Yasin Radi <yasin.ben.hamman@gmail.com>
2023-04-09 16:28:05 +0200

    feature/disassembler Move opcodes to disassembler.

--- src/config.py
+++
@@ -1,6 +0,0 @@
-"""Configuration variables."""
-
-import requests
-
-
-OPCODES = requests.get("https://gbdev.io/gb-opcodes/Opcodes.json").json()
---
+++ src/emulator/disassembler/config.py
@@ -0,0 +1,11 @@
+"""Configuration variables."""
+
+import requests
+
+from utils import parse_opcodes
+
+
+OPCODES = requests.get("https://gbdev.io/gb-opcodes/Opcodes.json").json()
+
+PREFIXED = parse_opcodes(OPCODES["cbprefixed"])
+INSTRUCTIONS = parse_opcodes(OPCODES["unprefixed"])
